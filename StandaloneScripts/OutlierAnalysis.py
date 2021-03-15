b49b12d21554aa42a449bddb7ae789af338b0fd4
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2021-03-15 22:29:44 +0100

    Update OutlierAnalysis.py

--- StandaloneScripts/OutlierAnalysis.py
+++ StandaloneScripts/OutlierAnalysis.py
@@ -14,19 +14,29 @@ lFt = ['DR', 'DS', 'NR', 'NS']
 cSep = ';'
 sCSV = 'csv'
 sTXT = 'txt'
-sP_CSV = os.path.join('..', '..', '..', '12_SysBio02_DataAnalysis',
-                      '02_ProcessedRawData', '01_CSV')
+lSPR_Base = ['..', '..', '..', '12_SysBio02_DataAnalysis']
+lSPR_Dat = ['02_ProcessedRawData', '01_CSV']
+lSPR_Res = ['04_OutlierResults']
 S_2L = 'log2'
 S_UN = 'unTransformed'
 
 # --- INPUT -------------------------------------------------------------------
-cTr = S_2L
+cTrans = S_2L           # S_2L / S_UN
 lThrDef = [3., 3.5, 4., 4.5, 5., 6., 8.]
+useIQR = False          # calculate MAD with IQR if True, otherwise use median
+
 nDigRnd = 4
+sFDatSt = 'Metabolite'
 sFResultFull = 'FullOutlierResult'
-sFNumOcc = 'NumOccurrences'
+sFResultDfr = 'OutlierThresholds'
+sFNumOcc = 'NumOccOutlier'
+lSColSkip = ['MeanConc']
 
 # --- DERIVED VALUES ----------------------------------------------------------
+sMode = 'median'
+if useIQR:
+    sMode = 'IQR'
+lSPR_Dat, lSPR_Res = lSPR_Base + lSPR_Dat, lSPR_Base + lSPR_Res
 dDAttr = {}
 for cGT, cNmGT in dGT.items():
     dDAttr[cGT] = {cFt: [cFt + '_' + dGT[cGT] + '_' + str(k) for k in
@@ -37,6 +47,35 @@ for cGT, cNmGT in dGT.items():
             dDAttr[cGT][cFt] = [cFt + '_' + dGT[cGT] + '_' + str(k) for k in
                                 range(1, 5 + 1)]
 dThrDef = {k: cThr for k, cThr in enumerate(lThrDef)}
+
+# --- OS FUNCTIONS ------------------------------------------------------------
+def makeDirs(pDTarget):
+    if not os.path.isdir(pDTarget):
+        os.makedirs(pDTarget)
+
+def joinToPath(lD4P=[], sF=None):
+    if len(lD4P) > 0:
+        pD = ''
+        for cD in lD4P:
+            pD = os.path.join(pD, cD)
+        makeDirs(pD)
+        if sF is None:
+            return pD
+        else:
+            return os.path.join(pD, sF)
+    else:
+        if sF is None:
+            return '.'
+        else:
+            return sF
+
+def getPFDat(sFDatSt, lD4P=[], dGT=dGT, sTr=cTrans, cGT='GT0', sFExt=sCSV):
+    sFDat = sFDatSt + '_' + sTr + '_' + cGT + '_' + dGT[cGT] + '_ThS'
+    return joinToPath(lD4P=lD4P, sF=sFDat + '.' + sFExt)
+
+def getPFRes(sFResSt, lD4P=[], sTr=cTrans, sMd=sMode, sFExt=sCSV):
+    sFRes = sFResSt + '_' + sTr + '_' + sMd + '_' + cGT
+    return joinToPath(lD4P=lD4P, sF=sFRes + '.' + sFExt)
 
 # --- GENERAL FUNCTIONS -------------------------------------------------------
 def addToDictCt(cD, cK, cCt=1):
@@ -88,23 +127,33 @@ def is_outlier(points, thresh=3.5):
     diff = np.sqrt(diff)
     med_abs_deviation = np.median(diff)
 
-    modified_z_score = 0.67449 * diff/med_abs_deviation
+    modified_z_score = 0.67449*diff/med_abs_deviation
 
     return modified_z_score > thresh
 
-def getOutliersMAD(pdSer, dThr=dThrDef):
-    cMedian = pdSer.median()
-    cDiff = np.sqrt((pdSer - cMedian)**2)
-    medAbsDev = np.median(cDiff)
-    zScoreMod = 0.67449 * cDiff/medAbsDev
+def calcMadIQR(pdSer, qtLB=0.25, qtUB=0.75):
+    npArr = pdSer.to_numpy()
+    IQR = np.quantile(npArr, qtUB, axis=0) - np.quantile(npArr, qtLB, axis=0)
+    # as the IQR may be zero, using the variance avoids inf
+    if IQR == 0:
+        IQR = np.var(npArr)
+    return np.median(np.abs(npArr)/IQR)
+
+def getOutliersMAD(pdSer, dThr=dThrDef, useIQR=False, zFact=0.67449):
+    cDiff = np.sqrt((pdSer - pdSer.median())**2)
+    if useIQR:
+        medAbsDev = calcMadIQR(pdSer)
+    else:
+        medAbsDev = np.median(cDiff)
+    zScoreMod = zFact*cDiff/medAbsDev
     return {k: (cThr, pdSer[zScoreMod > cThr]) for k, cThr in dThr.items()}
 
-def analyseDataFrame(pdDfr, dAttr):
+def analyseDataFrame(pdDfr, dAttr, useIQR=False):
     dResDat, dResThr, dNOcc = {}, {}, {}
     for sI in pdDfr.index:
         for sAttr, lAttr in dAttr.items():
             cSer = pdDfr.loc[sI, lAttr]
-            dResDat[(sI, sAttr)] = getOutliersMAD(cSer)
+            dResDat[(sI, sAttr)] = getOutliersMAD(cSer, useIQR=useIQR)
     for tK, cDOutl in dResDat.items():
         for k, tV in cDOutl.items():
             dVOutl = tV[1].to_dict()
@@ -127,9 +176,8 @@ def printOutliers(dResThr, cGT):
             print(tSet, ':', dOutl)
     print('='*16, cGT, '|', 'END', '='*16)
 
-def saveFullOutlierResult(dResThr, cGT, sFResSt, nDgRd, sFEnd=sCSV, cSp=cSep):
-    sFRes = sFResSt + '_' + cGT + '.' + sFEnd
-    fRes = open(sFRes, mode='w')
+def saveFullOutlierResult(sPRes, dResThr, cGT, nDgRd, cSp=cSep):
+    fRes = open(sPRes, mode='w')
     for dOutl in dResThr.values():
         for (cKO, (cThr, cDO)) in dOutl.items():
             fRes.write(str(cThr) + cSp + cKO[0] + cSp + cKO[1])
@@ -137,6 +185,17 @@ def saveFullOutlierResult(dResThr, cGT, sFResSt, nDgRd, sFEnd=sCSV, cSp=cSep):
                 fRes.write(cSp + cKI + cSp + str(round(cVI, nDgRd)))
             fRes.write('\n')
     fRes.close()
+
+def saveDataFrameThr(sPRes, dResThr, inpDfr, cGT, lSCSkip=None, cSp=cSep):
+    lCOut = [s for s in inpDfr.columns if s not in lSCSkip]
+    outDfr = pd.DataFrame(0., index=inpDfr.index, columns=lCOut)
+    for dOutl in dResThr.values():
+        for (cKO, (cThr, cDO)) in dOutl.items():
+            assert cKO[0] in outDfr.index
+            for cKI in cDO:
+                assert cKI in outDfr.columns
+                outDfr.at[cKO[0], cKI] = cThr
+    outDfr.to_csv(sPRes, sep=cSp, na_rep='NA')
 
 def getFirstLine(dAttr, lF=lFt, cSp=cSep):
     sP = 'Threshold'
@@ -163,10 +222,8 @@ def getDataLine(dNOccThr, dAttr, k=0, lF=lFt, dThr=dThrDef, cSp=cSep):
             sL += addToLineStr(dNOccThr, sM, cSp=cSp)
     return sL
 
-def saveNumOcc(dNOcc, dAttr, cGT, sFResSt, lF=lFt, dThr=dThrDef, sFEnd=sCSV,
-               cSp=cSep):
-    sFRes = sFResSt + '_' + cGT + '.' + sFEnd
-    fRes = open(sFRes, mode='w')
+def saveNumOcc(sPRes, dNOcc, dAttr, cGT, lF=lFt, dThr=dThrDef, cSp=cSep):
+    fRes = open(sPRes, mode='w')
     fRes.write(getFirstLine(dAttr, lF=lF, cSp=cSp))
     for i, dNOccThr in dNOcc.items():
         sLD = getDataLine(dNOccThr, dAttr, i, lF=lF, dThr=dThr, cSp=cSp)
@@ -174,17 +231,20 @@ def saveNumOcc(dNOcc, dAttr, cGT, sFResSt, lF=lFt, dThr=dThrDef, sFEnd=sCSV,
     fRes.close()
 
 # --- MAIN --------------------------------------------------------------------
-print('*'*16, 'START', '*'*16)
+print('*'*16, 'START', cTrans, 'with mode', sMode, '*'*16)
 # for cGT in ['GT0']:
 for cGT in dGT:
-    sF_CSV = 'Metabolite_' + cTr + '_' + cGT + '_' + dGT[cGT] + '_ThS'
-    pFDat = os.path.join(sP_CSV, sF_CSV + '.' + sCSV)
+    pFDat = getPFDat(sFDatSt, lSPR_Dat, dGT, sTr=cTrans, cGT=cGT, sFExt=sCSV)
     cDfr = pd.read_csv(pFDat, sep=cSep, index_col=0)
-    dResData, dResThres, dNumOcc = analyseDataFrame(cDfr, dDAttr[cGT])
+    dResData, dResThres, dNumOcc = analyseDataFrame(cDfr, dDAttr[cGT], useIQR)
     # printOutliers(dResThres, cGT)
-    saveFullOutlierResult(dResThres, cGT, sFResultFull, nDigRnd)
-    saveNumOcc(dNumOcc, dDAttr[cGT], cGT, sFNumOcc)
+    pFRes = getPFRes(sFResultFull, lSPR_Res, sTr=cTrans, sMd=sMode, sFExt=sCSV)
+    saveFullOutlierResult(pFRes, dResThres, cGT, nDgRd=nDigRnd, cSp=cSep)
+    pFRes = getPFRes(sFResultDfr, lSPR_Res, sTr=cTrans, sMd=sMode, sFExt=sCSV)
+    saveDataFrameThr(pFRes, dResThres, cDfr, cGT, lSCSkip=lSColSkip, cSp=cSep)
+    pFRes = getPFRes(sFNumOcc, lSPR_Res, sTr=cTrans, sMd=sMode, sFExt=sCSV)
+    saveNumOcc(pFRes, dNumOcc, dDAttr[cGT], cGT, lFt, dThr=dThrDef, cSp=cSep)
     print('Saved results for genotype', cGT + '.')
-print('*'*16, 'DONE', '*'*16)
+print('*'*16, 'DONE', cTrans, 'with mode', sMode, '*'*16)
 
 # -----------------------------------------------------------------------------
